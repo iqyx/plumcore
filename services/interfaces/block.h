53d604228d622cc740379c63b958fae8d2eb63cf
Marek Koza <qyx@krtko.org>
2021-03-06 06:11:51 +0100

    Modify block device storage interface

--- services/interfaces/block.h
+++ services/interfaces/block.h
@@ -1,51 +1,41 @@
-/*
+/* SPDX-License-Identifier: BSD-2-Clause
+ *
  * Block memory device interface descriptor
  *
- * Copyright (c) 2016, Marek Koza (qyx@krtko.org)
+ * Copyright (c) 2021, Marek Koza (qyx@krtko.org)
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #pragma once
 
 #include <stdint.h>
-#include <stdbool.h>
 
+#include "interface.h"
 
 typedef enum {
-	IBLOCK_RET_OK = 0,
-	IBLOCK_RET_FAILED,
-	IBLOCK_RET_NULL,
-	IBLOCK_RET_BAD_ARG,
-	IBLOCK_RET_USED,
-} iblock_ret_t;
+	BLOCK_RET_OK = 0,
+	BLOCK_RET_FAILED,
+	BLOCK_RET_NULL,
+	BLOCK_RET_OUT_OF_RANGE,
+} block_ret_t;
 
 
-typedef struct iblock {
-	uint32_t client_refs;
-	uint32_t host_refs;
-	bool opened_for_writing;
+typedef struct {
+	block_ret_t (*get_block_size)(void *parent, size_t *block_size);
+	block_ret_t (*set_block_size)(void *parent, size_t block_size);
+	block_ret_t (*get_size)(void *parent, size_t *size);
+	block_ret_t (*read)(void *parent, size_t block, uint8_t *buf, size_t len);
+	block_ret_t (*write)(void *parent, size_t block, const uint8_t *buf, size_t len);
+	block_ret_t (*flush)(void *parent);
+} block_vmt_t;
 
-} IBlock;
+typedef struct {
+	block_vmt_t *vmt;
+	void *parent;
+} Block;
 
+
+block_ret_t block_init(Block *self, block_vmt_t *vmt);
+block_ret_t block_free(Block *self);
 
 
