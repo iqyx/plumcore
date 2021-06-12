282229dc533505f8b4f81aeb4235cc544459cdc7
Marek Koza <qyx@krtko.org>
2021-06-12 13:18:24 +0200

    Refactor the filesystem interface

--- services/interfaces/fs.h
+++ services/interfaces/fs.h
@@ -1,79 +1,89 @@
-/*
+/* SPDX-License-Identifier: BSD-2-Clause
+ *
  * Generic filesystem interface
  *
- * Copyright (c) 2018, Marek Koza (qyx@krtko.org)
+ * Copyright (c) 2018-2021, Marek Koza (qyx@krtko.org)
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #pragma once
 
 #include <stdint.h>
-#include "interface.h"
+
+typedef enum fs_mode {
+	FS_MODE_NONE = 0,
+	FS_MODE_APPEND = (1 << 0),
+	FS_MODE_TRUNCATE = (1 << 1),
+	FS_MODE_CREATE = (1 << 2),
+	FS_MODE_READONLY = (1 << 3),
+	FS_MODE_WRITEONLY = (1 << 4),
+	FS_MODE_READWRITE = (1 << 5),
+} fs_mode_t;
+
+typedef enum fs_ret {
+	FS_RET_OK = 0,
+	FS_RET_FAILED,
+	FS_RET_NULL,
+} fs_ret_t;
+
+typedef enum fs_seek {
+	FS_SEEK_SET,
+	FS_SEEK_CUR,
+	FS_SEEK_END,
+} fs_seek_t;
+
+typedef struct fs_file {
+	uint32_t handle;
+	void *ptr;
+} File;
+
+typedef struct fs_dir {
+	void *ptr;
+} Dir;
+
+typedef struct fs_stat {
+	uint32_t handle;
+	char *name;
+	size_t name_size;
+	size_t size;
+} fs_stat_t;
+
+typedef struct fs_info {
+	size_t size_total;
+	size_t size_used;
+} fs_info_t;
+
+typedef struct fs_dir_entry {
+	uint32_t handle;
+	void *ptr;
+	char *name;
+	size_t name_size;
+	size_t size;
+} DirEntry;
 
 
-#define IFS_MODE_APPEND 1
-#define IFS_MODE_TRUNCATE 2
-#define IFS_MODE_CREATE 4
-#define IFS_MODE_READONLY 8
-#define IFS_MODE_WRITEONLY 16
-#define IFS_MODE_READWRITE 32
-
-typedef enum {
-	IFS_RET_OK = 0,
-	IFS_RET_FAILED,
-	IFS_RET_NULL,
-} ifs_ret_t;
-
-typedef uint32_t IFsFile;
-struct ifs_vmt {
-	ifs_ret_t (*open)(void *context, IFsFile *f, const char *filename, uint32_t mode);
-	ifs_ret_t (*close)(void *context, IFsFile f);
-	ifs_ret_t (*remove)(void *context, const char *filename);
-	ifs_ret_t (*rename)(void *context, const char *old_fn, const char *new_fn);
-	ifs_ret_t (*read)(void *context, IFsFile f, uint8_t *buf, size_t len, size_t *read);
-	ifs_ret_t (*write)(void *context, IFsFile f, const uint8_t *buf, size_t len, size_t *written);
-	ifs_ret_t (*flush)(void *context, IFsFile f);
-	ifs_ret_t (*stats)(void *context, size_t *total, size_t *used);
-
-	void *context;
+typedef struct fs Fs;
+struct fs_vmt {
+	fs_ret_t (*open)(Fs *self, File *f, const char *path, enum fs_mode mode);
+	fs_ret_t (*read)(Fs *self, File *f, void *buf, size_t len, size_t *read);
+	fs_ret_t (*write)(Fs *self, File *f, const void *buf, size_t len, size_t *written);
+	fs_ret_t (*lseek)(Fs *self, File *f, size_t offset, enum fs_seek whence);
+	fs_ret_t (*remove)(Fs *self, const char *path);
+	fs_ret_t (*fremove)(Fs *self, File *f);
+	fs_ret_t (*stat)(Fs *self, const char *path, struct fs_stat *s);
+	fs_ret_t (*fstat)(Fs *self, File *f, struct fs_stat *s);
+	fs_ret_t (*fflush)(Fs *self, File *f);
+	fs_ret_t (*close)(Fs *self, File *f);
+	fs_ret_t (*rename)(Fs *self, const char *old_path, const char *new_path);
+	fs_ret_t (*info)(Fs *self, struct fs_info *info);
+	fs_ret_t (*opendir)(Fs *self, Dir *d, const char *path);
+	fs_ret_t (*closedir)(Fs *self, Dir *d);
+	fs_ret_t (*readdir)(Fs *self, Dir *d, DirEntry *e);
 };
 
-typedef struct ifs {
-	Interface interface;
+typedef struct fs {
+	const struct fs_vmt *vmt;
+	void *parent;
+} Fs;
 
-	struct ifs_vmt vmt;
-} IFs;
 
-
-ifs_ret_t ifs_init(IFs *self);
-ifs_ret_t ifs_free(IFs *self);
-
-ifs_ret_t ifs_fread(IFs *self, IFsFile f, uint8_t *buf, size_t len, size_t *read);
-ifs_ret_t ifs_fwrite(IFs *self, IFsFile f, const uint8_t *buf, size_t len, size_t *written);
-ifs_ret_t ifs_fflush(IFs *self, IFsFile f);
-ifs_ret_t ifs_fclose(IFs *self, IFsFile f);
-
-ifs_ret_t ifs_open(IFs *self, IFsFile *f, const char *filename, uint32_t mode);
-ifs_ret_t ifs_remove(IFs *self, const char *filename);
-ifs_ret_t ifs_rename(IFs *self, const char *old_fn, const char *new_fn);
-ifs_ret_t ifs_stats(IFs *self, size_t *total, size_t *used);
