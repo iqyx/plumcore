0bcc049e1d3be3d55f5604d2f9dc589d891375f9
Marek Koza <qyx@krtko.org>
2021-06-19 13:57:23 +0200

    Tidy up the main include file, add scheduler includes

--- system/main.h
+++ system/main.h
@@ -1,40 +1,27 @@
-/*
+/* SPDX-License-Identifier: BSD-2-Clause
+ *
  * plumCore entry point and main system include file
  *
- * Copyright (c) 2015-2018, Marek Koza (qyx@krtko.org)
+ * Copyright (c) 2015-2021, Marek Koza (qyx@krtko.org)
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #pragma once
 
+/* Configuration generated from the Kconfig's .config. Must be the first. */
 #include "config.h"
+
 #include "platform.h"
 #include "port.h"
 #include "system.h"
 
+/* Scheduler and IPC includes */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
+#include "stream_buffer.h"
 
+/* Global helper routines */
 #include "u_assert.h"
 #include "u_log.h"
 
