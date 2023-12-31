f4e4611e7b4333738a4cb63d3bdf2512a821061b
Marek Koza <qyx@krtko.org>
2023-12-31 22:10:03 +0100

    Fix documentation excludes

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@ extensions = [
 numfig = True
 autosectionlabel_prefix_document = True
 templates_path = ['templates']
-exclude_patterns = ['build', 'static', 'lib2/duktape', 'lib/other/zfp']
+exclude_patterns = ['build', 'static', 'lib/duktape', 'lib/zfp']
 
 html_theme = 'sphinx_rtd_theme'
 html_title = 'plumCore IoT/DAQ framework'
