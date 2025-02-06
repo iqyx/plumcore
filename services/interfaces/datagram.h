5dbedf557332cdba3834f0260812783ab05e7c1d
Marek Koza <qyx@krtko.org>
2025-02-06 13:16:31 +0100

    feat(iface/datagram): Modify Datagram interface

--- services/interfaces/datagram.h
+++ services/interfaces/datagram.h
@@ -1,8 +1,8 @@
-/* SPDX-License-Identifier: BSD-2-Clause
+/* SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Generic bidirectional datagram interface
  *
- * Copyright (c) 2024, Marek Koza (qyx@krtko.org)
+ * Copyright (c) 2024-2025, Marek Koza (qyx@krtko.org)
  * All rights reserved.
  */
 
@@ -11,24 +11,36 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/** @todo Datagram interface is basically identical to the Stream interface. Those two are different in
- *        the way how framing is handled. Datagram interface always sends/receives the whole datagram
- *        in a single method call.
+/**
+ * @brief Datagram interface
+ *
+ * Datagram interface is basically identical to the Stream interface. Those two are different in
+ * the way how framing is handled. Datagram interface always sends/receives the whole datagram
+ * in a single method call.
  */
 
 typedef enum datagram_ret {
 	DATAGRAM_RET_OK = 0,
 	DATAGRAM_RET_FAILED,
 	DATAGRAM_RET_TIMEOUT,
-	DATAGRAM_RET_EOF,
 } datagram_ret_t;
+
+/**
+ * @brief Additional datagram metadata
+ */
+struct datagram_msg {
+	size_t addr_size;
+	uint8_t *local_addr;
+	uint32_t local_port;
+	uint8_t *remote_addr;
+	uint32_t remote_port;
+};
 
 typedef struct datagram Datagram;
 struct datagram_vmt {
-	stream_ret_t (*write)(Stream *self, const void *buf, size_t size);
-	stream_ret_t (*read)(Stream *self, void *buf, size_t size, size_t *read);
-	stream_ret_t (*write_timeout)(Stream *self, const void *buf, size_t size, size_t *written, uint32_t timeout_ms);
-	stream_ret_t (*read_timeout)(Stream *self, void *buf, size_t size, size_t *read, uint32_t timeout_ms);
+	datagram_ret_t (*write)(Datagram *self, const void *buf, size_t len, const struct datagram_msg *msg);
+	datagram_ret_t (*read)(Datagram *self, void *buf, size_t *len, struct datagram_msg *msg);
+
 };
 
 typedef struct datagram {
