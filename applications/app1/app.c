e89748c3ec7aa5f76476dd7a7325cd37db4f3911
Marek Koza <qyx@krtko.org>
2025-11-15 22:07:30 +0100

    fix(app/app1): Fix compilation warnings and documentation

--- applications/app1/app.c
+++ applications/app1/app.c
@@ -1,3 +1,11 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Basic application template
+ *
+ * Copyright (c) 2025, Marek Koza (qyx@krtko.org)
+ * All rights reserved.
+ */
+
 #include <main.h>
 #include "app.h"
 
@@ -11,5 +19,6 @@ app_ret_t app_init(App *self) {
 
 
 app_ret_t app_free(App *self) {
+	(void)self;
 	return APP_RET_OK;
 }
--- applications/app1/app.h
+++ applications/app1/app.h
@@ -1,3 +1,11 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Basic application template
+ *
+ * Copyright (c) 2025, Marek Koza (qyx@krtko.org)
+ * All rights reserved.
+ */
+
 #pragma once
 
 #include "config.h"
@@ -8,15 +16,27 @@ typedef enum {
 } app_ret_t;
 
 typedef struct {
+	/* Struct cannot be empty (-Wpedantic) */
+	uint32_t foo;
 } App;
 
 
 /**
  * @brief Prepare the new instance, allocate resources and start
  *        the application task.
+ *
  * @param self Preallocated memory for the instance
- * @return APP_RET_OK if the task was started successfully, error otherwise
+ * @return APP_RET_FAILED if the application initialization failed,
+ *         APP_RET_OK otherwise
  */
 app_ret_t app_init(App *self);
+
+/**
+ * @brief Free the application instance, release all allocated resources
+ *
+ * @param self Instance of the application
+ * @return APP_RET_FAILED on error,
+ *         APP_RET_OK otherwise.
+ */
 app_ret_t app_free(App *self);
 
