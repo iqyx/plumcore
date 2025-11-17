257a355339f129dc6a5b325f0581ee6ed7237c1e
Marek Koza <qyx@krtko.org>
2025-11-17 01:23:05 +0100

    feat(srv/stm32-gpio): Implement STM32 MCU GPIOv2 driver service

--- services/stm32-gpio/stm32-gpio.c
+++ services/stm32-gpio/stm32-gpio.c
@@ -2,7 +2,7 @@
  *
  * STM32 GPIO driver
  *
- * Copyright (c) 2024, Marek Koza (qyx@krtko.org)
+ * Copyright (c) 2025, Marek Koza (qyx@krtko.org)
  * All rights reserved.
  */
 
@@ -18,21 +18,170 @@
 #define MODULE_NAME "stm32-gpio"
 
 
-stm32_gpio_ret_t stm32_gpio_init(
-	Stm32Gpio *self,
-	enum stm32_port port,
-	enum stm32_pin pin,
-	enum gpio_mode mode,
-	enum gpio_otype otype,
-	enum gpio_pull pull
-) {
+/**********************************************************************************************************************
+ * GPIO interface implementation
+ **********************************************************************************************************************/
 
+static gpio_ret_t gpio_set(Gpio *gpio, bool state) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	if (state) {
+		STM32_GPIO_BSRR(self->port) |= (1 << pin);
+	} else {
+		STM32_GPIO_BSRR(self->port) |= (1 << (pin + 16));
+	}
+
+	return GPIO_RET_OK;
+}
+
+
+static gpio_ret_t gpio_get(Gpio *gpio, bool *state) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	if (state != NULL) {
+		if (STM32_GPIO_ODR(self->port) & (1 << pin)) {
+			*state = true;
+		} else {
+			*state = false;
+		}
+		return GPIO_RET_OK;
+	}
+
+	return GPIO_RET_FAILED;
+}
+
+
+static gpio_ret_t gpio_toggle(Gpio *gpio) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	uint32_t state = STM32_GPIO_ODR(self->port);
+	STM32_GPIO_BSRR(self->port) = ((state & (1 << pin)) << 16) | (~state & (1 << pin));
+
+	return GPIO_RET_OK;
+}
+
+
+static gpio_ret_t gpio_set_mode(Gpio *gpio, enum gpio_mode mode) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	uint32_t pinmoder = 0;
+	switch (mode) {
+		case MODE_OUTPUT: pinmoder = 1; break;
+		case MODE_ALTERNATE: pinmoder = 2; break;
+		case MODE_ANALOG: pinmoder = 3; break;
+		default: break;
+	}
+	STM32_GPIO_MODER(self->port) = (STM32_GPIO_MODER(self->port) & ~(3 << (pin * 2))) | (pinmoder << (pin * 2));
+
+	return GPIO_RET_OK;
+}
+
+
+static gpio_ret_t gpio_set_pinmux(Gpio *gpio, uint32_t mux) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	if (pin < 8) {
+		STM32_GPIO_AFRL(self->port) = (STM32_GPIO_AFRL(self->port) & ~(0xf << (pin * 4))) | (mux << (pin * 4));
+	} else {
+		pin -= 8;
+		STM32_GPIO_AFRH(self->port) = (STM32_GPIO_AFRH(self->port) & ~(0xf << (pin * 4))) | (mux << (pin * 4));
+	}
+
+	return GPIO_RET_OK;
+}
+
+
+static gpio_ret_t gpio_set_pull(Gpio *gpio, enum gpio_pull pull) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	uint32_t pupd = 0;
+	switch (pull) {
+		case PULL_UP: pupd = 1; break;
+		case PULL_DOWN: pupd = 2; break;
+		default: break;
+	}
+	STM32_GPIO_PUPDR(self->port) = (STM32_GPIO_PUPDR(self->port) & ~(3 << (pin * 2))) | (pupd << (pin * 2));
+
+	return GPIO_RET_OK;
+}
+
+
+static gpio_ret_t gpio_set_otype(Gpio *gpio, enum gpio_otype otype) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	if (otype == OTYPE_OD) {
+		STM32_GPIO_OTYPER(self->port) |= (1 << pin);
+	} else {
+		STM32_GPIO_OTYPER(self->port) &= ~(1 << pin);
+	}
+
+	return GPIO_RET_OK;
+}
+
+
+static gpio_ret_t gpio_set_ospeed(Gpio *gpio, enum gpio_ospeed ospeed) {
+	Stm32Gpio *self = gpio->parent;
+	int pin = gpio - &(self->pin[0]);
+
+	uint32_t s = 0;
+	switch (ospeed) {
+		case OSPEED_LOW: break;
+		case OSPEED_MEDIUM: s = 1; break;
+		case OSPEED_HIGH: s = 2; break;
+		case OSPEED_VERYHIGH: s = 3; break;
+		default: break;
+	}
+	STM32_GPIO_OSPEEDR(self->port) = (STM32_GPIO_OSPEEDR(self->port) & ~(3 << (pin * 2))) | (s << (pin * 2));
+
+	return GPIO_RET_OK;
+}
+
+
+static const struct gpio_vmt stm32_gpio_vmt = {
+	.set = gpio_set,
+	.get = gpio_get,
+	.toggle = gpio_toggle,
+	.set_mode = gpio_set_mode,
+	.set_pinmux = gpio_set_pinmux,
+	.set_pull = gpio_set_pull,
+	.set_otype = gpio_set_otype,
+	.set_ospeed = gpio_set_ospeed,
+};
+
+
+/**********************************************************************************************************************
+ * Service implementation
+ **********************************************************************************************************************/
+
+stm32_gpio_ret_t stm32_gpio_init(Stm32Gpio *self, enum stm32_port port) {
+	memset(self, 0, sizeof(Stm32Gpio));
+	self->port = port;
+
+	/* Initialize Gpio pin interfaces. They are effectively the same
+	 * but they need to have distinctive addresses. */
+	for (int i = 0; i < 16; i++) {
+		self->pin[i].parent = self;
+		self->pin[i].vmt = &stm32_gpio_vmt;
+	}
+
+	u_log(system_log, LOG_TYPE_INFO, U_LOG_MODULE_PREFIX("16 pins initialized on port %p (GPIO%c)"),
+		port,
+		(port - STM32_GPIO_BASE) / 0x400u + 'A'
+	);
 
 	return STM32_GPIO_RET_OK;
 }
 
+
 stm32_gpio_ret_t stm32_gpio_free(Stm32Gpio *self) {
-
-
+	(void)self;
+	/* Nothing to free. */
 	return STM32_GPIO_RET_OK;
 }
--- services/stm32-gpio/stm32-gpio.h
+++ services/stm32-gpio/stm32-gpio.h
@@ -2,7 +2,7 @@
  *
  * STM32 GPIO driver
  *
- * Copyright (c) 2024, Marek Koza (qyx@krtko.org)
+ * Copyright (c) 2025, Marek Koza (qyx@krtko.org)
  * All rights reserved.
  */
 
@@ -10,56 +10,53 @@
 
 #include <interfaces/gpio.h>
 
+#if !(defined(STM32L4) || defined(STM32G4) || defined(STM32H7))
+#error "stm32-gpio service is not compatible with this MCU family"
+#endif
+
 typedef enum {
 	STM32_GPIO_RET_OK,
 	STM32_GPIO_RET_FAILED,
 } stm32_gpio_ret_t;
 
-enum stm32_port {
-	STM32_PORTA = (0 << 4),
-	STM32_PORTB = (1 << 4),
-	STM32_PORTC = (2 << 4),
-	STM32_PORTD = (3 << 4),
-	STM32_PORTE = (4 << 4),
-	STM32_PORTF = (5 << 4),
-	STM32_PORTG = (6 << 4),
-	STM32_PORTH = (7 << 4),
-	STM32_PORTI = (8 << 4),
-	STM32_PORTJ = (9 << 4),
-	STM32_PORTK = (10 << 4),
-};
+/**
+ * Registers of the GPIO v2 peripheral found in newer STM32s.
+ */
+#define STM32_GPIO_BASE 0x48000000U
+#define STM32_GPIO_MODER(port) MMIO32((port) + 0x00)
+#define STM32_GPIO_OTYPER(port) MMIO32((port) + 0x04)
+#define STM32_GPIO_OSPEEDR(port) MMIO32((port) + 0x08)
+#define STM32_GPIO_PUPDR(port) MMIO32((port) + 0x0c)
+#define STM32_GPIO_IDR(port) MMIO32((port) + 0x10)
+#define STM32_GPIO_ODR(port) MMIO32((port) + 0x14)
+#define STM32_GPIO_BSRR(port) MMIO32((port) + 0x18)
+#define STM32_GPIO_LCKR(port) MMIO32((port) + 0x1c)
+#define STM32_GPIO_AFRH(port) MMIO32((port) + 0x20)
+#define STM32_GPIO_AFRL(port) MMIO32((port) + 0x24)
 
-enum stm32_pin {
-	STM32_PIN0 = 0,
-	STM32_PIN1 = 1,
-	STM32_PIN2 = 2,
-	STM32_PIN3 = 3,
-	STM32_PIN4 = 4,
-	STM32_PIN5 = 5,
-	STM32_PIN6 = 6,
-	STM32_PIN7 = 7,
-	STM32_PIN8 = 8,
-	STM32_PIN9 = 9,
-	STM32_PIN10 = 10,
-	STM32_PIN11 = 11,
-	STM32_PIN12 = 12,
-	STM32_PIN13 = 13,
-	STM32_PIN14 = 14,
-	STM32_PIN15 = 15,
+/**
+ * @brief STM32 GPIO peripheral (port) base addresses
+ */
+enum stm32_port {
+	STM32_PORTA = STM32_GPIO_BASE + 0x0000U,
+	STM32_PORTB = STM32_GPIO_BASE + 0x0400U,
+	STM32_PORTC = STM32_GPIO_BASE + 0x0800U,
+	STM32_PORTD = STM32_GPIO_BASE + 0x0c00U,
+	STM32_PORTE = STM32_GPIO_BASE + 0x1000U,
+	STM32_PORTF = STM32_GPIO_BASE + 0x1400U,
+	STM32_PORTG = STM32_GPIO_BASE + 0x1800U,
+	STM32_PORTH = STM32_GPIO_BASE + 0x1c00U,
+	STM32_PORTI = STM32_GPIO_BASE + 0x2000U,
+	STM32_PORTJ = STM32_GPIO_BASE + 0x2400U,
+	STM32_PORTK = STM32_GPIO_BASE + 0x2800U,
 };
 
 
 typedef struct {
-
+	enum stm32_port port;
+	Gpio pin[16];
 
 } Stm32Gpio;
 
-stm32_gpio_ret_t stm32_gpio_init(
-	Stm32Gpio *self,
-	enum stm32_port port,
-	enum stm32_pin pin,
-	enum gpio_mode mode,
-	enum gpio_otype otype,
-	enum gpio_pull pull
-);
+stm32_gpio_ret_t stm32_gpio_init(Stm32Gpio *self, enum stm32_port port);
 stm32_gpio_ret_t stm32_gpio_free(Stm32Gpio *self);
