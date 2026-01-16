73fc3817ec643e3f0485216b50a29f64e95da93e
Marek Koza <qyx@krtko.org>
2026-01-16 10:18:59 +0100

    Fix port_task_timer_init implicit declaration

--- ports/nwdaq-if-rs44/freertos-port.h
+++ ports/nwdaq-if-rs44/freertos-port.h
@@ -4,6 +4,8 @@
 
 
 extern uint32_t SystemCoreClock;
+void port_task_timer_init(void);
+uint32_t port_task_timer_get_value(void);
 #define configCPU_CLOCK_HZ (SystemCoreClock)
 #define configTICK_RATE_HZ ((TickType_t)CONFIG_FREERTOS_TICK_RATE_HZ)
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS port_task_timer_init
